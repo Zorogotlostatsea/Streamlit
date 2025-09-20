97bad5332521aca566e13f65f17f9160e4dd507e
Zorogotlostatsea <click2ahmedsalman@gmail.com>
2025-09-20 21:36:45 +0500

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title('Website name')
+st.subheader('Welcome!')
+if st.button('click'):
+    st.write('Hello There!')
+tab1, tab2 = st.columns(2)
+
+with tab1:
+    st.write('This website will guide you')
+with st.sidebar:
+    st.subheader('What we offer:')
+    st.write('Get your work done easily')
+
+    st.write('- We provide only the best services')
+    st.write('- We excel and exceed exceptionally')
+    st.write('- Subscribe to our program now!')
+    st.write('---')
+    st.subheader('Contact us now')
+    st.write('- Mobile: +92 3242700848')
+    st.write('- Email: click2ahmedsalman@gmail.com')
+    st.write('---')
